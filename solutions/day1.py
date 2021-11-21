21e27849901791b98fd2b263452c03a6651c1d7e
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-11-21 12:49:22 -0500

    setup

---
+++ solutions/day1.py
@@ -0,0 +1,10 @@
+def day1():
+    data = [int(x) for x in open(r'inputs\day1.in')]
+    print('Part 1 result: ' + str(part1(data)))
+    print('Part 2 result: ' + str(part2(data)))
+
+def part1(data):
+    return 0
+
+def part2(data):
+    return 0
