a193b8301ea5d09cf35daa24580b25e6567c8dde
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-12-01 00:07:19 -0500

    day 1

--- solutions/day1.py
+++ solutions/day1.py
@@ -4,7 +4,24 @@ def day1():
     print('Part 2 result: ' + str(part2(data)))
 
 def part1(data):
-    return 0
+    sum = 0
+    for i, val in enumerate(data):
+        if i == 0:
+            continue
+        else:
+            if val > data[i-1]:
+                sum += 1
+    return sum
 
 def part2(data):
-    return 0
+    sum = 0
+    for i, val in enumerate(data):
+        if len(data) < i+4:
+            break
+        three_window_sum = val + data[i+1] + data[i+2]
+        next_sum = data[i+1] + data[i+2] + data[i+3]
+        if next_sum > three_window_sum:
+            sum += 1
+    return sum
+
+day1()
