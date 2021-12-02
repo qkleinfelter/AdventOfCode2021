34c5f8488b1cba89bbdb2cd6342a3911063923e2
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-12-02 14:49:27 -0500

    day 2

---
+++ solutions/day2.py
@@ -0,0 +1,41 @@
+def day1():
+    data = open(r'inputs\day2.in').readlines()
+    print('Part 1 result: ' + str(part1(data)))
+    print('Part 2 result: ' + str(part2(data)))
+
+
+def part1(data):
+    horiz = 0
+    depth = 0
+    for line in data:
+        pieces = line.split()
+        dir = pieces[0]
+        amt = int(pieces[1])
+        if dir == "forward":
+            horiz += amt
+        elif dir == "up":
+            depth -= amt
+        elif dir == "down":
+            depth += amt
+    return horiz * depth
+
+
+def part2(data):
+    horiz = 0
+    depth = 0
+    aim = 0
+    for line in data:
+        pieces = line.split()
+        dir = pieces[0]
+        amt = int(pieces[1])
+        if dir == "forward":
+            horiz += amt
+            depth += aim * amt
+        elif dir == "up":
+            aim -= amt
+        elif dir == "down":
+            aim += amt
+    
+    return horiz * depth
+
+day1()
