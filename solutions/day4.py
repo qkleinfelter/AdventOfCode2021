b7b369c974349b10f1516be7b41211b17a8c57ff
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-12-04 19:27:59 -0500

    part 2

--- solutions/day4.py
+++ solutions/day4.py
@@ -1,8 +1,8 @@
 def solution():
-    data = [x for x in open(r'inputs\test.in').read().strip().split("\n")]
-    number_order = open(r'inputs\test.in').readline().strip().split(',')
+    data = [x for x in open(r'inputs\day4.in').read().strip().split("\n")]
+    number_order = open(r'inputs\day4.in').readline().strip().split(',')
     print('Part 1 result: ' + str(part1(data, number_order)))
-    print('Part 2 result: ' + str(part2(data)))
+    print('Part 2 result: ' + str(part2(data, number_order)))
 
 
 def part1(data, number_order):
@@ -39,8 +39,44 @@ def part1(data, number_order):
 
     return 0
 
-def part2(data):
-    return 0
+def part2(data, number_order):
+    data = data[2:]
+    data = [x for x in data if x != '']
+    board_count = int(len(data) / 5)
+    boards = []
+    for i in range(board_count):
+        board = []
+        for x in range(5):
+            row = [int(num) for num in data[i * 5 + x].split()]
+            board.append(row)
+        boards.append(board)
+
+    winning_boards = {}
+    last_win_index = -1
+    for i in range(len(number_order)):
+        # loop through all the numbers in the list
+        current_number = number_order[i]
+        for boardindex, board in enumerate(boards):
+            if boardindex in winning_boards:
+                continue
+            for rowindex in range(5):
+                row = board[rowindex]
+                for colindex in range(5):
+                    if row[colindex] == int(current_number):
+                        row[colindex] = -1
+                        # cannot be any bingos if less than 5 numbers have beencalled
+                        if (i < 5):
+                            continue
+                        if has_bingo(board):
+                            unmarked = 0
+                            for j in range(5):
+                                for k in range(5):
+                                    if (board[j][k] != -1):
+                                        unmarked += board[j][k]
+                            winning_boards[boardindex] = unmarked * int(current_number)
+                            last_win_index = boardindex
+
+    return winning_boards[last_win_index]
 
 
 def has_bingo(board):
